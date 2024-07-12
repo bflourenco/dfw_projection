9b4dba5a834108c78a0adc2c1aca62165920429e
Bruno <bruno@ism.ac.jp>
2024-07-12 21:28:55 +0900

    The code for generating part of the experimental data was commented out in 'experiments_batch_hyper.m' so I uncommented the corresponding lines

--- experiments/experiments_batch_hyper.m
+++ experiments/experiments_batch_hyper.m
@@ -1,15 +1,18 @@
 tStart = tic;
 ex_proj_hyper_table(10,1,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(10,1,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
+ex_proj_hyper_table(10,2,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
+ex_proj_hyper_table(10,2,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(20,1,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(20,1,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
+ex_proj_hyper_table(20,2,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
+ex_proj_hyper_table(20,2,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(30,27,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(30,27,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(40,37,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(40,37,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(50,47,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 ex_proj_hyper_table(50,47,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
-
 % 
 % ex_proj_hyper_table(10,2,30,1e-3,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
 % ex_proj_hyper_table(10,2,30,1e-8,[1e-1,5e-2,1e-2,5e-3,1e-3,5e-4,1e-4,5e-5,1e-5],true)
